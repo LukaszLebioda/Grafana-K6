817a9c76d2b12d2eb8a1f33a51b16b26ffca3d3f
llebioda <llebioda@novomatic-tech.com>
2024-05-26 09:19:00 +0200

    load vs stress

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ k6 run filename.js
 vu => virtual user
 
 latency => time between sending request and getting response
-
+ramp up / ramp down - increase / decrease of the number of users; in this phase we test the scalability of the system
 scaling => expanding the system's ability to handle growing number of traffic
 vertical scaling => improving a server (adding more memory, using faster CPU) (less elastic, sometimes sits idle, better for monolitic architecture)
 horizontal scaling => adding more servers (more elastic and reasonable, but better for microservice architecture)
@@ -25,7 +25,7 @@ service-level objective (SLO) => established limits of an app, e.g.
 
 # performance test types
 
-SMOKE => check if the app (or script) is working and has no fundamental issues
-LOAD => many users at concurrently
-STRESS => verify how many users our app can handle before it breaks
+SMOKE => check if the app (or script) is working and has no fundamental issues; it's just a glance at the app
+LOAD => many users concurrently, but the load should be typical (as designed); tests last longer; number of users increase / decrease gradually; this is the basic type of tests, that should be run after every app change
+STRESS => verify how many users our app can handle before it breaks; we take a closer look at the ramp down to see if the app can recover
 SPIKE => suddenly increase / decrease the load on the system
--- test_performance.js
+++ test_performance.js
@@ -1,17 +1,70 @@
-// SMOKE
-import http from 'k6/http'
-import { sleep } from 'k6'
+// // SMOKE TEST
+// import http from 'k6/http'
+// import { sleep } from 'k6'
 
-export const options = {
-	vus: 1,
-	duration: '3s',
-}
+// export const options = {
+// 	vus: 1,
+// 	duration: '3s',
+// }
 
-export default function () {
-	http.get('https://test.k6.io')
-	sleep(1)
-	http.get('https://test.k6.io/contacts.php')
-	sleep(2)
-	http.get('https://test.k6.io/news.php')
-	sleep(2)
-}
+// export default function () {
+// 	http.get('https://test.k6.io')
+// 	sleep(1)
+// 	http.get('https://test.k6.io/contacts.php')
+// 	sleep(2)
+// 	http.get('https://test.k6.io/news.php')
+// 	sleep(2)
+// }
+
+//----------
+
+// // LOAD TEST
+// import http from 'k6/http'
+// import { sleep } from 'k6'
+
+// export const options = {
+// 	stages: [
+// 		{
+// 			duration: '5m',
+// 			target: 100, // ramp up
+// 		},
+// 		{
+// 			duration: '30m',
+// 			target: 100, // duration
+// 		},
+// 		{
+// 			duration: '5m',
+// 			target: 0, // ramp down
+// 		},
+// 	],
+// }
+
+// export default function () {
+// 	http.get('https://test.k6.io')
+// 	sleep(1)
+// }
+
+// ---------
+
+// // STRESS TEST
+// import http from 'k6/http'
+// import { sleep } from 'k6'
+
+// export const options = {
+// 	stages: [
+// 		{
+// 			duration: '5m',
+// 			target: 1000, // ramp up - much more vu's
+// 		},
+// 		{
+// 			duration: '30m',
+// 			target: 1000, // duration
+// 		},
+// 		{
+// 			duration: '5m',
+// 			target: 0, // ramp down - critical
+// 		},
+// 	],
+// }
+
+//----------
