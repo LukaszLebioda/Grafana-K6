cdf5560e8d08e471449094cd3d102f4edcd0fdac
Łukasz Lebioda <llebioda@novomatic-tech.com>
2025-01-02 20:04:03 +0100

    custom metric recap

--- testFile.js
+++ testFile.js
@@ -5,6 +5,7 @@
 import http from "k6/http";
 import { check } from "k6";
 import { sleep } from "k6";
+import { Counter, Trend } from "k6/metrics"; // for custom metrics
 
 export const options = {
 	vus: 10,
@@ -17,10 +18,13 @@ export const options = {
 		iterations: ["count>10"], // counter: at least 20 iterations have to be made
 		http_req_failed: ["rate<0.01"], // rate: only 1% requests can fail
 		http_reqs: ["rate>3"], // rate: at least 4 reqs/sec should be sent
-		vus: ["value>1"], // gauge: there should be at least 1 vu at every moment
+		// vus: ["value>1"], // gauge: there should be at least 1 vu at every moment
 		vus_max: ["value < 20"], // gauge: there should be max 20 vu at every moment
-		// unique treshold for checks
+		// unique treshold for checks (check functions)
 		checks: ["rate>=0.98"], // rate: 98% cheks have to pass
+		// unique treshold for custom metrics
+		my_counter_metric: ["count>10"],
+		my_trend_metric: ["p(95)<500", "p(99)<1000"],
 	},
 	// 	stages: [
 	// 		{ duration: "10s", target: 10 },
@@ -29,8 +33,12 @@ export const options = {
 	// 	],
 };
 
+// for custom metrics
+let myCounter = new Counter("my_counter_metric");
+let myTrend = new Trend("my_trend_metric");
+
 export default function () {
-	const response = http.get("https://test.k6.io/");
+	let response = http.get("https://test.k6.io/"); // let because it is re-used later; if not, use const
 	// console.log(response.status, response.body);
 
 	check(response, {
@@ -38,7 +46,15 @@ export default function () {
 		"body contains header": (res) => res.body.includes("load testing"),
 	});
 
+	// for custom metrics 1
+	myCounter.add(1); // counts how any times default function has been entered
 	sleep(1); // 1 sec pause after each iteration
 	// http.get("https://test.k6.io/contacts.php");
 	// sleep(2);
+
+	// for custom metric 2
+	response = http.get("https://test.k6.io/news.php");
+	console.log(response.timings.duration);
+	myTrend.add(response.timings.duration); // measures response timings for /news.php
+	sleep(1);
 }
